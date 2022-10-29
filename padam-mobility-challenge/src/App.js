745d071d96d7cc479976dffa8de3318431d00757
sabanyilmaz1 <syilmaz1>
2022-10-29 17:17:41 +0200

    Installation React Bootstrap

--- padam-mobility-challenge/src/App.js
+++ padam-mobility-challenge/src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return <div className="App">Recherche de trajets</div>;
+  const [stops, setStops] = useState([]);
+
+  const getAllStops = () => {
+    fetch("https://6130d11c8066ca0017fdaa97.mockapi.io/stops").then(
+      (response) =>
+        response.json().then((responseJSON) => setStops(responseJSON))
+    );
+  };
+
+  useEffect(() => {
+    getAllStops();
+  });
+
+  return (
+    <div className="App">
+      <Container className="p-3">
+        Recherche de trajets
+        <div>
+          {stops.map((stop, index) => (
+            <div key={index}>{stop}</div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
